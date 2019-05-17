1c67bd3fc45382372c5b6b3a0688220291818615
Arif Hidayat <arifhidayat1010@gmail.com>
2019-05-17 16:21:05 +0700

    update component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BatchComponent } from './batch/batch.component';
+import { BackofficeComponent } from './backoffice/backoffice.component';
+import { TrainerComponent } from './trainer/trainer.component';
+import { StudyMaterialComponent } from './study-material/study-material.component';
+import { StudyPeriodeComponent } from './study-periode/study-periode.component';
+import { AppRoutingModule } from './app-routing.module';
+import { SharedComponent } from './shared/shared.component';
+import { BootcampPeriodComponent } from './bootcamp-period/bootcamp-period.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BatchComponent,
+    BackofficeComponent,
+    TrainerComponent,
+    StudyMaterialComponent,
+    StudyPeriodeComponent,
+    SharedComponent,
+    BootcampPeriodComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
